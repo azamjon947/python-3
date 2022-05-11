6e4a7930cc2fea50c29e56677cbb40583ce39e6c
azamjon947 <105347025+azamjon947@users.noreply.github.com>
2022-05-11 15:40:30 +0500

    Add files via upload

---
+++ python 3/main.py
@@ -0,0 +1,17 @@
+
+
+
+harorat = int(input('Harorat necha gardus?: '))
+if harorat > 30:
+    print('Bugun issiq bo\'ladi!')
+else:
+    print("Bugun havo yaxshi!")
+
+harorat = int(input('Harorat necha gardus?: '))
+if harorat > 30:
+    print('Bugun issiq bo\'ladi!')
+elif harorat > 20:
+    print("Bugun havo ochiq bo'ladi")
+elif harorat > 10:
+    print("Bugun havo sovuq")
+print('Bajarildi!')
